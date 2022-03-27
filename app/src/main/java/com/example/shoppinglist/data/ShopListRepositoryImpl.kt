541b96c0165f0a34890ea47bd287866141aa3b0c
nizkiyVorobey <formobileprima@gmail.com>
2022-03-27 14:51:12 +0300

    add presentation layer

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
@@ -9,9 +11,16 @@ import java.lang.RuntimeException
  * Це потрібно, щоб не вийшло так, що на одному екрані ми працюємо з одним репозиторієм, а на іншому з іншим
  */
 object ShopListRepositoryImpl : ShopListRepository {
-
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0..10) {
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(item: ShopItem) {
         if (item.id == ShopItem.UNDEFINED_ID) {
@@ -19,6 +28,7 @@ object ShopListRepositoryImpl : ShopListRepository {
             autoIncrementId++
         }
         shopList.add(item)
+        updateList()
     }
 
     override fun editShopItem(item: ShopItem) {
@@ -29,14 +39,20 @@ object ShopListRepositoryImpl : ShopListRepository {
 
     override fun deleteShopItem(item: ShopItem) {
         shopList.remove(item)
+        updateList()
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList() // .toList create copy of shopList. This more safety, we can't mutate the collection out of this class
+    override fun getShopList(): LiveData<List<ShopItem>> {
+//        return shopList.toList() // .toList create copy of shopList. This more safety, we can't mutate the collection out of this class
+        return shopListLD
     }
 
     override fun getShopItem(id: Int): ShopItem {
         return shopList.find { it.id == id }
             ?: throw RuntimeException("Element with id $id not found")
+    }
+
+    private fun updateList() {
+        shopListLD.postValue(shopList.toList())
     }
 }
